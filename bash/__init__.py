7dee96a2687fefb84476ad466d0e13bc3adbc244
2947872572@qq.com <Wangchen100>
2019-04-12 18:21:32 +0800

    update

--- bash/__init__.py
+++ bash/__init__.py
@@ -1,3 +1,26 @@
 print("helloworld");
+# #########################字符串操作####################################
 message = "hello python world";
 print(message);
+# python中可以使用单引号和双引号定义字符串 便于 字符串中包含单引号和双引号  赞
+str = "'你'好";
+print(str);
+
+#快速转换大小写
+message = "hello python world";
+messageS = "HELLO WORLD";
+# 单词首字母全部转化为大写  title()
+print(message.title());
+# 单词全部转换为大写  upper()
+print(messageS.upper());
+#单词全部转换为小写 lower()
+print(messageS.lower());
+
+# 字符串拼接
+# 直接
+first_nema = "w";
+lost_name = "cc";
+full_name = first_nema+""+lost_name;
+print(full_name);
+
+3.
